c2774a9cd15c0db8a13b2f445c0863672fd29c2b
zemogmarq <zemogmarq@gmail.com>
2025-10-24 23:02:42 -0600

    Mi visualización Potree para el artículo

---
+++ pointclouds/Summit/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 1866984,
+    "boundingBox": {
+        "lx": 417116.0,
+        "ly": 2083964.0,
+        "lz": 2122.909912109375,
+        "ux": 417487.0,
+        "uy": 2084335.0,
+        "uz": 2493.909912109375
+    },
+    "tightBoundingBox": {
+        "lx": 417116.0,
+        "ly": 2083964.0,
+        "lz": 2122.909912,
+        "ux": 417306.0,
+        "uy": 2084335.0,
+        "uz": 2186.870117
+    },
+    "pointAttributes": "LAZ",
+    "spacing": 3.21295428276062,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
